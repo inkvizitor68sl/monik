9c5d9d13bc0684d0ca7d63351017b5f3a261b9db
Zhivotnev Vladislav <inkvizitor68sl@yandex-team.ru>
2016-12-27 19:48:22 +0300

    removed build/*

--- build/lib.linux-x86_64-2.7/monik/api/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Provide API realisation."""
-
-import flask
-
-from ..models.check import Check as CheckModel
-from ..models.checklist import CheckList as CheckListModel
-from ..helpers.command import Command
